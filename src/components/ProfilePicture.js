99b0f480d8b6b4320ff5c417e8aa47fc0025c3e9
joenapper <joe.napper@hotmail.co.uk>
2021-10-12 13:48:29 +0100

    Made ProfilePicture size dynamic

--- src/components/ProfilePicture.js
+++ src/components/ProfilePicture.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import {View, Image, StyleSheet} from 'react-native';
 
-const HomeScreen = ({uri}) => {
+const HomeScreen = ({uri, size = 70}) => {
   return (
-    <View style={styles.container}>
+    <View style={[styles.container, {width: size + 6, height: size + 6}]}>
       <Image
-        style={styles.image}
+        style={[styles.image, {width: size, height: size}]}
         source={{
           uri,
         }}
@@ -16,16 +16,12 @@ const HomeScreen = ({uri}) => {
 
 const styles = StyleSheet.create({
   container: {
-    height: 76,
-    width: 76,
     margin: 5,
     borderRadius: 40,
     borderWidth: 3,
     borderColor: '#ae22e0',
   },
   image: {
-    height: 70,
-    width: 70,
     borderRadius: 40,
     borderWidth: 1,
     borderColor: '#ffffff',
