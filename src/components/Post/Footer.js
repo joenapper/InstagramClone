55f26e21472e7cd8e1d05900b068ad7a6c4fb235
joenapper <joe.napper@hotmail.co.uk>
2021-10-13 15:25:40 +0100

    Installed and using vector-icons package

--- src/components/Post/Footer.js
+++ src/components/Post/Footer.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
+import ADIcon from 'react-native-vector-icons/AntDesign';
+import Fontisto from 'react-native-vector-icons/Fontisto';
+import IoniconsIcon from 'react-native-vector-icons/Ionicons';
+import FAIcon from 'react-native-vector-icons/FontAwesome';
 
 const Footer = ({likesCount, caption, postedAt}) => {
   return (
     <View style={styles.container}>
+      <View style={styles.iconContainer}>
+        <View style={styles.iconGroup}>
+          <ADIcon name="hearto" size={25} color={'#545454'} />
+          <Fontisto name="comment" size={23} color={'#545454'} />
+          <IoniconsIcon
+            name="paper-plane-outline"
+            size={25}
+            color={'#545454'}
+          />
+        </View>
+        <FAIcon name="bookmark-o" size={25} color={'#545454'} />
+      </View>
       <Text style={styles.likesCount}>{likesCount} Likes</Text>
       <Text style={styles.caption}>{caption}</Text>
       <Text style={styles.postedAt}>{postedAt}</Text>
@@ -14,6 +30,17 @@ const Footer = ({likesCount, caption, postedAt}) => {
 const styles = StyleSheet.create({
   container: {
     margin: 5,
+  },
+  iconContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: 5,
+  },
+  iconGroup: {
+    flexDirection: 'row',
+    width: 120,
+    justifyContent: 'space-between',
   },
   likesCount: {
     fontWeight: 'bold',
--- src/components/Post/Header.js
+++ src/components/Post/Header.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
+import Icon from 'react-native-vector-icons/Entypo';
 
 import ProfilePicture from '../ProfilePicture';
 
 const Header = ({uri, name}) => {
   return (
     <View style={styles.container}>
-      <ProfilePicture uri={uri} size={40} />
-      <Text style={styles.name}>{name}</Text>
+      <View style={styles.wrapper}>
+        <ProfilePicture uri={uri} size={40} />
+        <Text style={styles.name}>{name}</Text>
+      </View>
+      <Icon name="dots-three-vertical" size={16} style={styles.icon} />
     </View>
   );
 };
@@ -16,10 +20,18 @@ const styles = StyleSheet.create({
   container: {
     flexDirection: 'row',
     alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  wrapper: {
+    flexDirection: 'row',
+    alignItems: 'center',
   },
   name: {
     fontWeight: 'bold',
     color: '#3c3c3c',
+  },
+  icon: {
+    marginRight: 10,
   },
 });
 
