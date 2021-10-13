10813c4020a10bf7c56cf152627a2148809eb5ad
joenapper <joe.napper@hotmail.co.uk>
2021-10-13 15:44:19 +0100

    Added like functionality

--- src/components/Post/Footer.js
+++ src/components/Post/Footer.js
@@ -1,16 +1,34 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {View, TouchableWithoutFeedback, Text, StyleSheet} from 'react-native';
 import ADIcon from 'react-native-vector-icons/AntDesign';
 import Fontisto from 'react-native-vector-icons/Fontisto';
 import IoniconsIcon from 'react-native-vector-icons/Ionicons';
 import FAIcon from 'react-native-vector-icons/FontAwesome';
 
-const Footer = ({likesCount, caption, postedAt}) => {
+const Footer = ({likesCount: likesCountProp, caption, postedAt}) => {
+  const [isLiked, setIsLiked] = useState(false);
+  const [likesCount, setLikesCount] = useState(0);
+  const onLikePressed = () => {
+    const amount = isLiked ? -1 : 1;
+    setLikesCount(likesCount + amount);
+    setIsLiked(!isLiked);
+  };
+
+  useEffect(() => {
+    setLikesCount(likesCountProp);
+  }, []);
+
   return (
     <View style={styles.container}>
       <View style={styles.iconContainer}>
         <View style={styles.iconGroup}>
-          <ADIcon name="hearto" size={25} color={'#545454'} />
+          <TouchableWithoutFeedback onPress={onLikePressed}>
+            {isLiked ? (
+              <ADIcon name="heart" size={25} color={'#e73838'} />
+            ) : (
+              <ADIcon name="hearto" size={25} color={'#545454'} />
+            )}
+          </TouchableWithoutFeedback>
           <Fontisto name="comment" size={23} color={'#545454'} />
           <IoniconsIcon
             name="paper-plane-outline"
