d02d5e0e3df6fa451484afe9d0560f159202aa2e
joenapper <joe.napper@hotmail.co.uk>
2021-10-14 13:49:20 +0100

    Implemented navigation

--- App.js
+++ App.js
@@ -1,14 +1,28 @@
 import React from 'react';
-import {SafeAreaView, StatusBar} from 'react-native';
+import {StatusBar} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
 import HomeScreen from './src/screens/HomeScreen';
+import DiscoveryScreen from './src/screens/DiscoveryScreen';
+import CreatePostScreen from './src/screens/CreatePostScreen';
+import NotificationsScreen from './src/screens/NotificationsScreen';
+import ProfileScreen from './src/screens/ProfileScreen';
+
+const Tab = createBottomTabNavigator();
 
 const App = () => {
   return (
-    <SafeAreaView>
+    <NavigationContainer>
       <StatusBar />
-      <HomeScreen />
-    </SafeAreaView>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Discovery" component={DiscoveryScreen} />
+        <Tab.Screen name="Post" component={CreatePostScreen} />
+        <Tab.Screen name="Notifications" component={NotificationsScreen} />
+        <Tab.Screen name="Profile" component={ProfileScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 };
 
