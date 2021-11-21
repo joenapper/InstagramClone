c1021c66fa875e63dc1a6a750b6b41be8091366c
joenapper <joe.napper@hotmail.co.uk>
2021-11-21 20:57:08 +0000

    feat: create bottomTabNavigator

--- App.js
+++ App.js
@@ -2,6 +2,10 @@ import React from 'react';
 import {StatusBar} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Foundation from 'react-native-vector-icons/Foundation';
+import Feather from 'react-native-vector-icons/Feather';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 import HomeScreen from './src/screens/HomeScreen';
 import DiscoveryScreen from './src/screens/DiscoveryScreen';
@@ -15,7 +19,30 @@ const App = () => {
   return (
     <NavigationContainer>
       <StatusBar />
-      <Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={({route}) => ({
+          tabBarIcon: ({focused, color, size}) => {
+            if (route.name === 'Home') {
+              return <Foundation name="home" size={size} color={color} />;
+            }
+            if (route.name === 'Discovery') {
+              return <Feather name="search" size={size} color={color} />;
+            }
+            if (route.name === 'Post') {
+              return <Feather name="plus-square" size={size} color={color} />;
+            }
+            if (route.name === 'Notifications') {
+              return <AntDesign name="hearto" size={size} color={color} />;
+            }
+            if (route.name === 'Profile') {
+              return <Ionicons name="person-outline" size={size} color={color} />;
+            }
+          },
+          tabBarActiveTintColor: '#000',
+          tabBarInactiveTintColor: 'gray',
+          tabBarShowLabel: false,
+          // headerShown: false,
+        })}>
         <Tab.Screen name="Home" component={HomeScreen} />
         <Tab.Screen name="Discovery" component={DiscoveryScreen} />
         <Tab.Screen name="Post" component={CreatePostScreen} />
