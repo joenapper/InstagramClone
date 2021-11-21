24adcc8566611fe095aea4bb1d2a9ea737db7397
joenapper <joe.napper@hotmail.co.uk>
2021-11-21 23:12:39 +0000

    feat: more advanced navigation with StoryScreen

--- src/components/Story.js
+++ src/components/Story.js
@@ -1,14 +1,21 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {TouchableOpacity, Text, StyleSheet} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
 
 import ProfilePicture from '../components/ProfilePicture';
 
 const Story = ({uri, name}) => {
+  const navigation = useNavigation();
+
+  const onPress = () => {
+    navigation.navigate('Story');
+  };
+
   return (
-    <View>
+    <TouchableOpacity onPress={onPress}>
       <ProfilePicture uri={uri} />
       <Text style={styles.name}>{name}</Text>
-    </View>
+    </TouchableOpacity>
   );
 };
 
---
+++ src/screens/StoryScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+const StoryScreen = () => {
+  return (
+    <SafeAreaView>
+      <Text>StoryScreen</Text>
+    </SafeAreaView>
+  );
+};
+
+export default StoryScreen;
